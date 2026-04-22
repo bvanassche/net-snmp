21093ddbe3a72db3da1c31150aaa3785bf3c54a6
Moshe Kaplan <mosheekaplan@gmail.com>
2026-04-22 17:28:54 -0400

    snmpd: Limit number of lines to 1024 in extension output
    Prevent stack-buffer overflow if output is more than 1024 lines.
    
    [ bvanassche: edited patch description and fixed indentation ]

--- agent/mibgroup/agent/extend.c
+++ agent/mibgroup/agent/extend.c
@@ -371,6 +371,10 @@ extend_load_cache(netsnmp_cache *cache, void *magic)
         line_buf[ 0 ] = extension->output;
         for (cp=extension->output; *cp; cp++) {
             if (*cp == '\n') {
+                if (extension->numlines >=
+                    sizeof(line_buf) / sizeof(line_buf[0])) {
+                    break;
+                }
                 line_buf[ extension->numlines++ ] = cp+1;
             }
         }
